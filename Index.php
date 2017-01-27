157bf83204d2f4caaf4ae3703715d2e5a5237056
ninapellan <nina.pellan@eemi.com>
2017-01-27 10:05:10 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,3 +2,10 @@
 <html>
 <head>
 <title>Social Party</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+
+<body>
+<div class="header">
